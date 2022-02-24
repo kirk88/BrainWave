98635e34a18b0116ca7a0490c47e3d9894a75ad3
Kirk <481314821@qq.com>
2022-02-24 23:17:15 +0800

    Initial Commit

--- app/src/main/java/com/brain/wave/model/BleResponse.kt
+++ app/src/main/java/com/brain/wave/model/BleResponse.kt
@@ -89,8 +89,8 @@ fun ByteArray.parseBleResponse(): BleResponse? {
 }
 
 fun String.parseBleResponse(): BleResponse? {
-   val str =  if(!contains(",")){
-       replace("(.{2})".toRegex(), ",$1").substring(1)
+    val str = if (!contains(",")) {
+        replace("(.{2})".toRegex(), ",$1").substring(1)
     } else this
     val bytes = str.split(",").map { it.toInt(16).toByte() }.toByteArray()
     return bytes.parseBleResponse()
@@ -119,7 +119,7 @@ private fun ByteArray.toInt(order: ByteOrder): Int {
 }
 
 fun ByteArray.getInt24(order: ByteOrder): Int {
-    if(order == ByteOrder.LITTLE_ENDIAN){
+    if (order == ByteOrder.LITTLE_ENDIAN) {
         reverse()
     }
 
@@ -130,5 +130,5 @@ fun main() {
     val str =
         "ff,01,00,c1,fe,0c,19,fc,ff,ff,19,fc,ff,ff,f4,25,d2,b5,18,d2,65,13,36,00,a3,0b,11,12,d2,b8,0b,d2,c0,25,d2,83,18,d2,dc,18,36,c7,fe,0a,de,11,d2,87,0b,d2,e5,24,d2,a6,17,d2,08,25,36,88,1b,0a,00,11,d2,a9,0a,d2,14,24,d2,d8,16,d2,1f,33,36,dd,57,09,33,10,d2,db,09,d2,1a,24,d2,df,16,d2,03,34,36,f7,6a,09,3d,10,d2,e1,09,d2,86,24,d2,4d,17,d2,64,2c,36,ba,cf,09,b0,10,d2,50,0a,d2,ff,24,d2,c7,17,d2,6a,21,36,ff,2e,0a,21,11,d2,cb,0a,d2,b2,25,d2,79,18,d2,33,10,36,af,c6,0a,d4,11,d2,78,0b,d2,0e,26,d2,d2,18,d2,21,fd,35,ee,3b,0b,30,12,d2,d6,0b,d2,5e,26,d2,26,19,d2,de,f5,35,d6,0d,0b,87,12,d2,2b,0c,d2,01,00,00,00,aa,00,00,00,1a,20,98"
 
- println(str.parseBleResponse())
+    println(str.parseBleResponse())
 }
